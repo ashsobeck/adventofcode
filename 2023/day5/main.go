aab07a8e33a2eb070bb90d00f8fe93a6b4e7e2d1
Ashton Sobeck <asobeck99@gmail.com>
2023-12-05 20:54:51 -0500

    wip: day5 progress

--- 2023/day5/main.go
+++ 2023/day5/main.go
@@ -1,17 +1,60 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"regexp"
+	"strconv"
 	"strings"
 )
+
+type AlmanacMap struct {
+	DestStart   uint64
+	SourceStart uint64
+	RangeLength uint64
+}
 
 func main() {
 	file, _ := os.ReadFile("./day5_test.txt")
 
+	getNum := regexp.MustCompile(`\d+`)
+
 	newLine := func(c rune) bool {
-		return !(c == '\n')
+		return c == '\n' || c == ':'
 	}
 
 	almanac := strings.FieldsFunc(string(file), newLine)
-	for _, 
+	fmt.Println(almanac)
+	var seeds []string
+	var currentMap int
+	almanacMaps := make(map[int][]AlmanacMap)
+
+	for _, info := range almanac {
+		nums := getNum.FindAllString(info, -1)
+		if nums == nil {
+			currentMap++
+			continue
+		}
+		if currentMap == 1 {
+			seeds = append(seeds, nums...)
+		} else {
+			var vals []uint64
+			for _, val := range nums {
+				temp, _ := strconv.ParseUint(val, 10, 64)
+				vals = append(vals, temp)
+			}
+
+			almanacMaps[currentMap] = append(almanacMaps[currentMap], AlmanacMap{vals[0], vals[1], vals[2]})
+		}
+	}
+	fmt.Println(seeds)
+	fmt.Println(almanacMaps)
+
+	var convertedVal uint64
+	for _, seed := range seeds {
+		for _, convs := range almanacMaps {
+			var inRange bool
+
+		}
+	}
 }
